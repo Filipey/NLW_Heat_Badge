e621cb19c320ef448d06ef20e304fc113e534c32
Filipe Augusto Santos de Moura <85424389+Filipey@users.noreply.github.com>
2021-10-22 16:31:29 -0300

    Update main.js

--- Badge/main.js
+++ Badge/main.js
@@ -1,4 +1,4 @@
-const socialMedia = {
+const LinkSocialMedia = {
   github: 'Filipey',
   youtube: 'UC6dfD3ZcTuX0OFy6wqLx7NQ',
   facebook: 'zuck',
@@ -6,24 +6,27 @@ const socialMedia = {
   twitter: 'DzFilipeDz'
 }
 
-function changeSocialMedia() {
+function changeSocialMediaLinks() {
   for (let li of socialLinks.children) {
     const social = li.getAttribute('class')
-    li.children[0].href = `https://${social}.com/${socialMedia[social]}`
+
+    li.children[0].href = `https://${social}.com/${LinkSocialMedia[social]}`
   }
 }
 
-changeSocialMedia()
+changeSocialMediaLinks()
 
 function getGitHubProfileInfos() {
-  const url = `https://api.github.com/users/${SocialMedia.github}`
+  const url = `https://api.github.com/users/${LinkSocialMedia.github}`
 
   fetch(url)
     .then(response => response.json())
     .then(data => {
-      ;(userName.textContent = data.name)((userBio.textContent = data.bio))(
-        (userLink.href = data.html_url)
-      )((userImage.src = data.avatar_url))((userLogin.textContent = data.login))
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userImage.src = data.avatar_url
+      userLogin.textContent = data.login
     })
 }
 
